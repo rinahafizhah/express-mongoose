8f89cb36133b0c886f6f95633b486394168e9dd9
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-22 11:35:56 +0700

    add bodyParser

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const PORT = 3000;
 // Initial express
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 //initial mongoose
 const mongoose = require("mongoose");
