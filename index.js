f52ae77f62903e7cc2d8feeea8d56a4481f79a95
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-22 11:32:00 +0700

    adding timestamps in schema

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const PORT = 3000
+const PORT = 3000;
 
 // Initial express
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 //initial mongoose
-const mongoose = require("mongoose")
-const MONGODB_URI = `mongodb://localhost:27017/company`
-mongoose.connect(MONGODB_URI,{
+const mongoose = require("mongoose");
+const MONGODB_URI = `mongodb://localhost:27017/company`;
+mongoose.connect(
+  MONGODB_URI,
+  {
     useNewUrlParser: true
-})
+  }
+);
 
 app.listen(PORT, () => {
-    console.log(`App running on PORT ${PORT}`)
-})
+  console.log(`App running on PORT ${PORT}`);
+});
 
-const Schema = mongoose.Schema
-const Customers = mongoose.model("customers", new Schema({
-    first_name: String,
-    last_name: String,
-    age: Number,
-    address: {}
-}))
+const Schema = mongoose.Schema;
+const Customers = mongoose.model(
+  "customers",
+  new Schema(
+    {
+      first_name: String,
+      last_name: String,
+      age: Number,
+      address: {
+        street: String,
+        city: String,
+        state: String
+      }
+    },
+    {
+      timestamps: true
+    }
+  )
+);
