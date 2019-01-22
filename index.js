9e9f321b38653d983012b1e31981b4d926ce491a
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-22 11:20:40 +0700

    add initialize data in mongoose

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const PORT = 3000
 
+// Initial express
 const express = require("express")
 const app = express()
+
+//initial mongoose
+const mongoose = require("mongoose")
+const MONGODB_URI = `mongodb://localhost:27017/company`
+mongoose.connect(MONGODB_URI,{
+    useNewUrlParser: true
+})
 
 app.listen(PORT, () => {
     console.log(`App running on PORT ${PORT}`)
 })
+
+const Schema = mongoose.Schema
+const Customers = mongoose.model("customers", new Schema({
+    first_name: String,
+    last_name: String,
+    age: Number,
+    address: {}
+}))
